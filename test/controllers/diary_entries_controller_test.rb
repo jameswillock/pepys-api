60547c3c90231878af09d9d1d4a648dcbf978532
James Willock <james.willock@gmail.com>
2019-01-21 16:28:58 +0000

    Add some tests

--- test/controllers/diary_entries_controller_test.rb
+++ test/controllers/diary_entries_controller_test.rb
@@ -2,12 +2,13 @@ require 'test_helper'
 
 class DiaryEntriesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get diary_entries_index_url
+    get diary_entries_url
     assert_response :success
   end
 
   test "should get show" do
-    get diary_entries_show_url
+    diary_entry = DiaryEntry.first
+    get diary_entry_url(diary_entry.id)
     assert_response :success
   end
 
--- test/models/diary_entry_test.rb
+++ test/models/diary_entry_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class DiaryEntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without a published_on date" do
+    diary_entry = DiaryEntry.new(text: "Example")
+    assert_raises(ActiveRecord::NotNullViolation) { diary_entry.save }
+  end
+
+  test "should not save without text" do
+    date = Date.parse("1660-01-01")
+    diary_entry = DiaryEntry.new(published_on: date)
+    assert_raises(ActiveRecord::NotNullViolation) { diary_entry.save }
+  end
+
+  test "should save correctly" do
+    date = Date.parse("1660-01-01")
+    diary_entry = DiaryEntry.new(published_on: date, text: "Example")
+    assert diary_entry.save
+  end
 end
